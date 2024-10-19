f894ded9b2bfd4da433be33dcc75d72a06ed8eef
le-anhquan123 <92944045+le-anhquan123@users.noreply.github.com>
2024-10-19 14:17:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bạn Nhung';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10-2024';
+const titleCard = 'Tặng Cao Mỹ Nhung';
+const contentCard = 'Chúc Nhung 20-10 vui vẻ và tràn ngập hạnh phúc nha';
+
+const giftImage = 'hoa.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
